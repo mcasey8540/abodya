003060ec9f82ac8d8861baa783a8170456697a1b
Michael Casey <mcasey8540@gmail.com>
2013-02-06 20:20:55 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abodya::Application.config.secret_token = '9c620b72552158efb36ead635ee4a29700d62f077be28786d99fc93959c8342c45737fbc43a86f9c5ddfd7fa3be29c5a03288621f1e71dafade742845be05afe'
