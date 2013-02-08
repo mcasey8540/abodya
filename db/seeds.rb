a856d41ca236cc3cc074562327f181d2d226839e
Michael Casey <mcasey8540@gmail.com>
2013-02-08 13:22:17 -0500

    Update seed.rb to add A,B,and C property owners

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ require 'open-uri'
 class MorrisCountyScraper
 
 	def initialize
-		#Q
-		@alpha_array = ["Y"]
+		#Q,Z,Y
+		@alpha_array = ["A", "B", "C"]
 		@record_url_array = []
 		@record_detail_url_array = []
 		@property_data_detail_array = []
